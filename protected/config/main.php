c85d74e0dbf8a2d2a29fc66e2e3bbe1bd90fcb40
Víctor Guzmán <fawques@gmail.com>
2013-03-02 01:22:28 +0100

    Añadido control de la BD

--- protected/config/main.php
+++ protected/config/main.php
@@ -70,6 +70,8 @@ return array(
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
+			'enableProfiling'=>true,
+        	'enableParamLogging'=>true,
 		),
 		
 		'errorHandler'=>array(
